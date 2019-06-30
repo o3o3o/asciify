2909a5f1e06a0f232b809dfc49137ece43526084
yahui <tianya0yingzi@gmail.com>
2019-06-30 18:33:19 +0800

    feat: replace the top most common pixel with blank

--- asciify.py
+++ asciify.py
@@ -1,66 +1,99 @@
 from PIL import Image
+from collections import Counter
 
-ASCII_CHARS = ['.',',',':',';','+','*','?','%','S','#','@']
+ASCII_CHARS = [".", ",", ":", ";", "+", "*", "?", "%", "S", "#", "@"]
 ASCII_CHARS = ASCII_CHARS[::-1]
 
-'''
+"""
 method resize():
     - takes as parameters the image, and the final width
     - resizes the image into the final width while maintaining aspect ratio
-'''
-def resize(image, new_width=100):
+"""
+
+
+def resize(image, new_width):
     (old_width, old_height) = image.size
-    aspect_ratio = float(old_height)/float(old_width)
+    aspect_ratio = float(old_height) / float(old_width)
     new_height = int(aspect_ratio * new_width)
     new_dim = (new_width, new_height)
     new_image = image.resize(new_dim)
     return new_image
-'''
+
+
+"""
 method grayscalify():
     - takes an image as a parameter
     - returns the grayscale version of image
-'''
-def grayscalify(image):
-    return image.convert('L')
+"""
 
-'''
+
+def grayscalify(image):
+    return image.convert("L")
+
+
+"""
 method modify():
     - replaces every pixel with a character whose intensity is similar
-'''
-def modify(image, buckets=25):
-    initial_pixels = list(image.getdata())
-    new_pixels = [ASCII_CHARS[pixel_value//buckets] for pixel_value in initial_pixels]
-    return ''.join(new_pixels)
+"""
 
-'''
+
+def modify(image, buckets=25, blank_thres_hold=0.9):
+    initial_pixels = list(image.getdata())
+    pixels = []
+    cnt = Counter()
+
+    for pixel_value in initial_pixels:
+        char = ASCII_CHARS[pixel_value // buckets]
+        cnt[char] += 1
+
+        pixels.append(char)
+
+    # Replace the top of [thres_hold] with blank
+    top_len = round(len(cnt) * (1 - blank_thres_hold))
+    most_common_pixels = cnt.most_common(top_len)
+    for tpx, _ in most_common_pixels:
+        new_pixels = [x if x != tpx else " " for x in pixels]
+        pixels = new_pixels
+
+    return "".join(pixels)
+
+
+"""
 method do():
     - does all the work by calling all the above functions
-'''
+"""
+
+
 def do(image, new_width=100):
-    image = resize(image)
+    image = resize(image, new_width=new_width)
     image = grayscalify(image)
 
     pixels = modify(image)
     len_pixels = len(pixels)
 
     # Construct the image from the character list
-    new_image = [pixels[index:index+new_width] for index in range(0, len_pixels, new_width)]
+    new_image = [
+        pixels[index : index + new_width] for index in range(0, len_pixels, new_width)
+    ]
 
-    return '\n'.join(new_image)
+    return "\n".join(new_image)
 
-'''
+
+"""
 method runner():
     - takes as parameter the image path and runs the above code
     - handles exceptions as well
     - provides alternative output options
-'''
+"""
+
+
 def runner(path):
     image = None
     try:
         image = Image.open(path)
     except Exception:
-        print("Unable to find image in",path)
-        #print(e)
+        print("Unable to find image in", path)
+        # print(e)
         return
     image = do(image)
 
@@ -71,19 +104,21 @@ def runner(path):
     # Note: This text file will be created by default under
     #       the same directory as this python file,
     #       NOT in the directory from where the image is pulled.
-    f = open('img.txt','w')
+    f = open("img.txt", "w")
     f.write(image)
     f.close()
 
-'''
+
+"""
 method main():
     - reads input from console
     - profit
-'''
-if __name__ == '__main__':
+"""
+if __name__ == "__main__":
     import sys
     import urllib.request
-    if sys.argv[1].startswith('http://') or sys.argv[1].startswith('https://'):
+
+    if sys.argv[1].startswith("http://") or sys.argv[1].startswith("https://"):
         urllib.request.urlretrieve(sys.argv[1], "asciify.jpg")
         path = "asciify.jpg"
     else:
